6b04d78cd71260fc3611c8de55f84e6535efccfa
Dani Tulp <danitulp@gmail.com>
2020-06-19 15:09:46 +0200

    bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 module.exports = [
   {
     input: "src/index.ts",
     plugins: [typescript()],
-    output: { dir: "build", format: "cjs" },
+    output: { file: pkg.main, format: "cjs" },
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
@@ -14,7 +14,7 @@ module.exports = [
     input: "src/index.ts",
     preserveModules: true,
     plugins: [typescript()],
-    output: { dir: "build", format: "es", sourcemap: true },
+    output: { dir: "build/lib", format: "es", sourcemap: true },
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
